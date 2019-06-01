9b4e7fdf63e84ab991580e18b96e0460121064e2
Arryexe <arianur098@gmail.com>
2019-06-02 04:38:44 +0800

    Make a layout for make a question and add model + migration for question

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,36 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">
+                    Ask Some Question !
+                    
+                    <div class="text-right" style="margin-top: -25px;">
+                        <a href="#">View All Question</a>
+                    </div>
+                </div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <form action="{{ url('question') }}" method="get">
+                        <table>
+                            <tr>
+                                <td>
+                                    <label for="qtitle" class="form-label">Question Title</label>
+                                    <input type="text" name="qtitle" id="qtitle" class="form-control">
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <label for="qcontent" class="form-label">Content (Optional)</label>
+                                    <textarea name="qcontent" class="form-control" rows="5" style="width: 720px;"></textarea>
+                                </td>
+                            </tr>
+                        </table>
+
+                        <br>
+
+                        <button type="submit" value="submit" class="btn btn-success btn-block">Submit Question</button>
+                    </form>
                 </div>
             </div>
         </div>
